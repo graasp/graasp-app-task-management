e84eff6bf9ddf7c1730cdb8449be655ec04d3458
Graciana <gracianaaad@hotmail.com>
2022-03-30 15:52:28 +0200

    add refetch

--- src/components/context/appData.js
+++ src/components/context/appData.js
@@ -2,13 +2,19 @@ import { useContext } from 'react';
 import { Context } from './ContextContext';
 import { TokenContext } from './TokenContext';
 import { hooks } from '../../config/queryClient';
+import { RE_FETCH_INTERVAL } from '../../constants/constants';
+
 
 export const useAppData = () => {
   const context = useContext(Context);
   const token = useContext(TokenContext);
-  const query = hooks.useAppData({ token, itemId: context?.get('itemId') });
+  const query = hooks.useAppData(
+    { token, itemId: context?.get('itemId') },
+    RE_FETCH_INTERVAL,
+  );
   return query;
 };
+
 
 export const useAppActions = () => {
   const context = useContext(Context);
@@ -21,5 +27,10 @@ export const useAppContext = () => {
   const context = useContext(Context);
   const token = useContext(TokenContext);
   const query = hooks.useAppContext({ token, itemId: context?.get('itemId') });
+  return query;
+};
+export const useAppSettingFile = (appSettingId, enabled) => {
+  const token = useContext(TokenContext);
+  const query = hooks.useAppSettingFile({ token, appSettingId }, { enabled });
   return query;
 };
