3a411653cca32d7c4d15de0e1f72e05cdb9e834b
Graciana <gracianaaad@hotmail.com>
2022-03-30 14:39:42 +0200

    remove test members

--- src/data/db.js
+++ src/data/db.js
@@ -3,54 +3,54 @@ import { v4 } from 'uuid';
 const buildDatabase = (appContext) => ({
   appData: [],
   members: [
+    // {
+    //   id: appContext.memberId,
+    //   name: appContext.memberName,
+    // },
     {
-      id: appContext.memberId,
-      name: appContext.memberName,
+      id: v4(),
+      name: 'Alex',
     },
-    // {
-    //   id: v4(),
-    //   name: 'Alex',
-    // },
-    // {
-    //   id: v4(),
-    //   name: 'Dana',
-    // },
-    // {
-    //   id: v4(),
-    //   name: 'Mira',
-    // },
-    // {
-    //   id: v4(),
-    //   name: 'Joe',
-    // },
-    // {
-    //   id: v4(),
-    //   name: 'Lynn',
-    // },
-    // {
-    //   id: v4(),
-    //   name: 'John',
-    // },
-    // {
-    //   id: v4(),
-    //   name: 'Mandy',
-    // },
-    // {
-    //   id: v4(),
-    //   name: 'Clara',
-    // },
-    // {
-    //   id: v4(),
-    //   name: 'Jean-Paul',
-    // },
-    // {
-    //   id: v4(),
-    //   name: 'Jean-Pierre',
-    // },
-    // {
-    //   id: v4(),
-    //   name: 'Anne-Marie',
-    // },
+    {
+      id: v4(),
+      name: 'Dana',
+    },
+    {
+      id: v4(),
+      name: 'Mira',
+    },
+    {
+      id: v4(),
+      name: 'Joe',
+    },
+    {
+      id: v4(),
+      name: 'Lynn',
+    },
+    {
+      id: v4(),
+      name: 'John',
+    },
+    {
+      id: v4(),
+      name: 'Mandy',
+    },
+    {
+      id: v4(),
+      name: 'Clara',
+    },
+    {
+      id: v4(),
+      name: 'Jean-Paul',
+    },
+    {
+      id: v4(),
+      name: 'Jean-Pierre',
+    },
+    {
+      id: v4(),
+      name: 'Anne-Marie',
+    },
  
   ],
 });
