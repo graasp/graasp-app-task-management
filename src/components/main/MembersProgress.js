3ed99e95b52f494c3fbc269dd98f478441cdb5bf
Graciana <gracianaaad@hotmail.com>
2022-04-20 16:32:28 +0200

    create all members panel

--- src/components/main/MembersProgress.js
+++ src/components/main/MembersProgress.js
@@ -14,9 +14,9 @@ import {
 } from 'recharts';
 
 const MembersProgress = ({ tasks, students }) => {
-  console.log(tasks, students);
   // in completed, I will see how many tasks are done by one student
-
+  const st=students[0].name;
+  console.log(st)
   const map1 = new Map();
   students.map((student) => map1.set(student.name, 0));
   const incrementCount = (label, arr, member) => {
@@ -34,57 +34,23 @@ const MembersProgress = ({ tasks, students }) => {
       });
     }
   } 
-  const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+
+ 
+  
+  // eslint-disable-next-line arrow-body-style
+  const arr = Array.from(map1, ([key, value]) => {
+    return {'name':key,'value':value*100};
+  });
+  
+  console.log(arr);
+  
 
   return (
     <ResponsiveContainer width="100%" height="100%">
       <BarChart
         width={500}
         height={300}
-        data={data}
+        data={arr}
         margin={{
           top: 5,
           right: 30,
@@ -97,8 +63,7 @@ const MembersProgress = ({ tasks, students }) => {
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="pv" fill="#8884d8" background={{ fill: '#eee' }} />
-        <Bar dataKey="uv" fill="#82ca9d" />
+        <Bar dataKey="value" fill="#8884d8" background={{ fill: '#eee' }} />
       </BarChart>
     </ResponsiveContainer>
   );
