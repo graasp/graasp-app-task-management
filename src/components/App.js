fc4a9853235b432fb5e74bfdcd7d714de02aec38
Graciana <gracianaaad@hotmail.com>
2022-04-25 15:19:44 +0200

    modify grid elements

--- src/components/App.js
+++ src/components/App.js
@@ -217,8 +217,7 @@ const App = () => {
           <DragDropContext onDragEnd={handleDragEnd}>
             <Grid
               container
-              rowSpacing={{ md: 2 }}
-              columnSpacing={{ xs: 1, sm: 2, md: 1 }}
+            
             >
               <Grid item xs={12} sm={12} md={12} lg={6} xl={4}>
                 {renderTasksList('To Do', TASK_LABELS.TODO, true)}
@@ -226,7 +225,7 @@ const App = () => {
               <Grid item xs={12} sm={12} md={12} lg={6} xl={4}>
                 {renderTasksList('In Progress', TASK_LABELS.IN_PROGRESS)}
               </Grid>
-              <Grid item xs={12} sm={12} md={12} lg={6} xl={4}>
+              <Grid item xs={12} sm={12} md={12} lg={6} xl={4} >
                 {renderTasksList('Completed', TASK_LABELS.COMPLETED)}
               </Grid>
             </Grid>
