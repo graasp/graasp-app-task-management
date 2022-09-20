b8e017660cbede13080b6d759e2eea46ac29aeff
Jérémy La Scala <jeremy.lascala@epfl.ch>
2022-09-20 12:21:02 +0200

    refactor: migrate FilterMembers to Mui5

--- src/components/main/settings/FilterMembers.js
+++ src/components/main/settings/FilterMembers.js
@@ -1,39 +1,19 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { makeStyles } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { Button } from '@graasp/ui';
-
-const useStyles = makeStyles(() => ({
-  toggleContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    width: '100%',
-  },
-
-  headerText: {
-    fontSize: '1.05vw',
-  },
-  headerDisabled: {
-    color: 'grey',
-  },
-  rightContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'column',
-  },
-}));
+import ToggleContainer from '../../common/ToggleContainer';
 
 const FilterMembers = ({ setToggle }) => {
-  const classes = useStyles();
   const { t } = useTranslation();
 
   return (
-    <div className={classes.toggleContainer}>
-      <Typography className={classes.headerText}>
+    <ToggleContainer>
+      <Typography sx={{
+    fontSize: '1.05vw',
+  }}>
         {t('Filter Members')}
       </Typography>
       <FormControlLabel
@@ -49,7 +29,7 @@ const FilterMembers = ({ setToggle }) => {
           </span>
         }
       />
-    </div>
+    </ToggleContainer>
   );
 };
 
