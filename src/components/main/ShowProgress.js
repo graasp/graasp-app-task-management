d32d93a6bb46ae92c2c2b932019d849074fcd716
Jérémy La Scala <jeremy.lascala@epfl.ch>
2022-09-20 12:12:26 +0200

    refactor: migrate ShowProgress  to Mui5

--- src/components/main/ShowProgress.js
+++ src/components/main/ShowProgress.js
@@ -1,30 +1,26 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { makeStyles } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { Button } from '@mui/material';
 
-const useStyles = makeStyles(() => ({
-  toggleContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    width: '100%',
-  },
-  headerText: {
-    fontSize: '1.05vw',
-  },
+const ToggleContainer = styled('div')(() => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  width: '100%',
 }));
 
 // eslint-disable-next-line react/prop-types
 const ShowProgress = ({ setToggle, toggle, handleModalClose }) => {
-  const classes = useStyles();
   const { t } = useTranslation();
 
   return (
-    <div className={classes.toggleContainer}>
-      <Typography className={classes.headerText}>
+    <ToggleContainer>
+      <Typography sx={{
+    fontSize: '1.05vw',
+  }}>
         {t('Track your progress.')}
       </Typography>
       <FormControlLabel
@@ -41,7 +37,7 @@ const ShowProgress = ({ setToggle, toggle, handleModalClose }) => {
           </Button>
         }
       />
-    </div>
+    </ToggleContainer>
   );
 };
 
