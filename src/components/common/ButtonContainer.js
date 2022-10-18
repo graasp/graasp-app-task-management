e865082358bcb73d2a749e6c7835a1e723469c76
Jérémy La Scala <jeremy.lascala@epfl.ch>
2022-10-18 17:41:56 +0200

    chore: final cleaning
    This commit might be dropped in future branches.

--- src/components/common/ButtonContainer.js
+++
@@ -1,8 +0,0 @@
-import { styled } from '@mui/material/styles';
-
-const ButtonContainer = styled('div')(() => ({
-  display: 'flex',
-  justifyContent: 'flex-end',
-}));
-
-export default ButtonContainer;
