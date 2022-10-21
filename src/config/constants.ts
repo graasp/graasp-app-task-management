7e3fd24dd132ac513d7600db721cc5a4766ae9aa
Jérémy La Scala <jeremy.lascala@epfl.ch>
2022-10-21 15:29:49 +0200

    chore: clean constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -12,24 +12,8 @@ export const GRAASP_APP_ID = process.env.REACT_APP_GRAASP_APP_ID;
 export const NODE_ENV =
   process.env.REACT_APP_NODE_ENV || process.env.NODE_ENV || ENV.DEVELOPMENT;
 
-export const DESCRIPTION_MAX_LENGTH = 30;
-
-export const DEFAULT_IMAGE_SRC =
-  'https://pbs.twimg.com/profile_images/1300707321262346240/IsQAyu7q_400x400.jpg';
-
-export const ROOT_ID = 'root-id';
-
-export const CONTAINER_HEIGHT = 450;
-
-export const DRAWER_WIDTH = 300;
 export const DEFAULT_LOCALE = 'en-US';
 export const DEFAULT_LANG = 'en';
-export const DEFAULT_SHOW_USER_SHARE_SETTING = true;
-
-export const HEADER_HEIGHT = 64;
-export const FILE_UPLOAD_MAX_FILES = 5;
-
-export const DEFAULT_PERMISSION = 'read';
 
 export const MOCK_API = REACT_APP_MOCK_API === 'true';
 
